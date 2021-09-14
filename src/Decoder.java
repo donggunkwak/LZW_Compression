963b48895b5ab767347b64236493e7a52e727244
donggunkwak <47698773+donggunkwak@users.noreply.github.com>
2021-09-14 10:46:12 -0700

    Fixing Decoder

--- src/Decoder.java
+++ src/Decoder.java
@@ -2,47 +2,87 @@ import java.util.*;
 import java.io.*;
 public class Decoder {
 
-	
+
 	//public static final int BITS = 12;
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		FileInputStream in = new FileInputStream("encoded.txt");//reader
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("decoded.txt")));
-		
+
 		StringBuilder ans = new StringBuilder();
 		byte[] bits = in.readAllBytes();
 		for(int i =0;i<bits.length;i++)
 		{
-			System.out.println(bits[i]+" "+toBinary(bits[i]));
-			out.print(toBinary(bits[i]));
+			//System.out.println(bits[i]+" "+toBinary(bits[i]));
+			//out.print(toBinary(bits[i]));
 		}
-		/*
-		while(true)
+
+		StringBuilder binary = new StringBuilder();
+		for(int i =0;i<bits.length;i++)
 		{
-			int inp = in.read();
-			if(inp==-1)
-				break;
-			/*if(inp>=256)
-				inp = inp%256;
-				
-			System.out.println(inp+" "+toBinary(inp));
-			//ans.append(toBinary(inp));
-			out.print(toBinary(inp));
-			
-		}*/
-	
-		/*for(int i =0;i<ans.length();)
+			binary.append(toBinary(bits[i]));
+		}
+
+		System.out.println(binary);
+		/*for(int i = 0 ;i<binary.length()-9;i+=9)
 		{
-			if(i+12>=ans.length())
+			System.out.print(binary.substring(i,i+9)+" ");
+		}System.out.println();
+		 */
+
+		ArrayList<Integer> binVals = new ArrayList<Integer>();
+
+		for(int i = 0 ;i<=binary.length()-12;i+=12)
+		{
+			binVals.add(binaryValue(binary.substring(i,i+12)));
+		}
+		System.out.println(binVals);
+
+		HashMap<Integer, String> dict = new HashMap<Integer,String>();
+		for(int i = 0;i<256;i++)
+			dict.put(i, ""+ (char)i);
+
+		int nextVal = 256;
+		int old = binVals.get(0);
+		out.print(dict.get(old));
+		String s = "";
+		String c = "";
+		for(int i = 1;i<binVals.size();i++)
+		{
+			int next = binVals.get(i);
+			if(!dict.containsKey(next))
 			{
-				out.println(ans.substring(i));
-				break;
+				s = dict.get(old);
+				s = s+c;
 			}
-			out.println(ans.substring(i,i+12));
-			i = i+12;
-		}*/
+			else
+			{
+				s = dict.get(next);
+			}
+			out.print(s);
+			c = ""+s.charAt(0);
+			dict.put(nextVal, dict.get(old)+c);
+			nextVal++;
+			old = next;
+		}
+
+		out.println();
+
 		out.close();
 
+
+	}
+	public static int binaryValue(String a)
+	{
+		int ans = 0;
+		for(int i = 0;i<12;i++)
+		{
+			if(a.charAt(i)=='1')
+			{
+				ans+=(1<<(11-i));
+			}
+		}
+		return ans;
 	}
 	public static String toBinary(int a)
 	{
